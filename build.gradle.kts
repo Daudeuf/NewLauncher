316ea8ebbfa89c520f7a5134e1d7b72e82825813
Clem76 <39047200+Daudeuf@users.noreply.github.com>
2025-04-21 14:35:38 +0200

    test 6

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,17 +32,6 @@ application {
     mainClass.set("fr.clem76.Main")
 }
 
-tasks.register<Exec>("analyzeModules") {
-    val javaHome = System.getenv("JAVA_HOME") ?: System.getProperty("java.home")
-    commandLine = listOf(
-        "$javaHome/bin/jdeps",
-        "--multi-release", "23",
-        "--ignore-missing-deps",
-        "--print-module-deps",
-        "build/libs/NewLauncher-all.jar"
-    )
-}
-
 javafx {
     version = "21"
     modules = listOf("javafx.swing", "javafx.web")
@@ -55,18 +44,74 @@ runtime {
     modules.set(listOf(
         "java.base",
         "java.compiler",
+        "java.datatransfer",
+        "java.desktop",
+        "java.instrument",
+        "java.logging",
+        "java.management",
+        "java.management.rmi",
+        "java.naming",
         "java.net.http",
+        "java.prefs",
+        "java.rmi",
         "java.scripting",
+        "java.se",
+        "java.security.jgss",
+        "java.security.sasl",
+        "java.smartcardio",
         "java.sql",
+        "java.sql.rowset",
+        "java.transaction.xa",
+        "java.xml",
+        "java.xml.crypto",
+        "jdk.accessibility",
+        "jdk.attach",
+        "jdk.charsets",
+        "jdk.compiler",
+        "jdk.crypto.cryptoki",
+        "jdk.crypto.ec",
+        "jdk.crypto.mscapi",
+        "jdk.dynalink",
+        "jdk.editpad",
+        "jdk.graal.compiler",
+        "jdk.graal.compiler.management",
+        "jdk.hotspot.agent",
+        "jdk.httpserver",
+        "jdk.incubator.vector",
+        "jdk.internal.ed",
+        "jdk.internal.jvmstat",
+        "jdk.internal.le",
+        "jdk.internal.md",
+        "jdk.internal.opt",
+        "jdk.internal.vm.ci",
+        "jdk.jartool",
+        "jdk.javadoc",
+        "jdk.jcmd",
+        "jdk.jconsole",
+        "jdk.jdeps",
+        "jdk.jdi",
+        "jdk.jdwp.agent",
         "jdk.jfr",
+        "jdk.jlink",
+        "jdk.jpackage",
+        "jdk.jshell",
         "jdk.jsobject",
+        "jdk.jstatd",
+        "jdk.localedata",
+        "jdk.management",
+        "jdk.management.agent",
+        "jdk.management.jfr",
+        "jdk.naming.dns",
+        "jdk.naming.rmi",
+        "jdk.net",
+        "jdk.nio.mapmode",
+        "jdk.sctp",
+        "jdk.security.auth",
+        "jdk.security.jgss",
         "jdk.unsupported",
         "jdk.unsupported.desktop",
         "jdk.xml.dom",
-
-        "java.desktop",
-        "jdk.crypto.ec",
-        "java.logging"
+        "jdk.zipfs"
     ))
 
     jpackage {
